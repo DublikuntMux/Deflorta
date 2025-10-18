08110958fa16fa8e2139c7162d4b399868d900c7
DublikuntMux <dublikunt@gmail.com>
2025-10-18 23:01:08 +0200

    add weighted array

---
+++ Deflorta/WeightedArray.hpp
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+#include "Base/Random.hpp"
+
+template <typename T>
+class WeightedArray {
+public:
+    struct Entry {
+        T value;
+        double weight;
+    };
+
+    void add(const T& value, double weight) {
+        if (weight <= 0.0) return;
+        entries_.push_back({ value, weight });
+        recomputePrefix();
+    }
+
+    const T& pick() const {
+        if (entries_.empty())
+            throw std::runtime_error("WeightedArray is empty!");
+
+        const double r = Random::UniformDouble(0.0, totalWeight_);
+        const auto it = std::ranges::upper_bound(prefixSums_, r);
+        std::size_t idx = std::distance(prefixSums_.begin(), it);
+
+        return entries_[idx].value;
+    }
+
+    double totalWeight() const noexcept { return totalWeight_; }
+
+private:
+    std::vector<Entry> entries_;
+    std::vector<double> prefixSums_;
+    double totalWeight_ = 0.0;
+
+    void recomputePrefix();
+};
